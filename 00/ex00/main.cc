75366c294eacc6df72eb9e02aa4661e9d1a1fead
iphuck22 <eldeshue@naver.com>
2024-07-05 18:03:45 +0900

    implement ex00 module00

---
+++ 00/ex00/main.cc
@@ -0,0 +1,27 @@
+
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+	std::string input("* LOUD AND UNBEARABLE FEEDBACK NOISE *");
+	if (argc <= 1)
+	{
+		std::cout << input;
+		return 0;
+	}
+	else
+	{
+		for (int i = 1; i < argc; ++i)
+		{
+			input = argv[i];
+			for (std::string::iterator itr = input.begin(); itr != input.end(); ++itr)
+			{
+				*itr = std::toupper(*itr);
+			}
+			std::cout << input;
+		}
+	}
+	std::cout << std::endl;
+	return 0;
+}
