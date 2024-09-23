8e72f5363c7a99b58e9ad0fc364fe65da73a7e88
iphuck22 <eldeshue@naver.com>
2024-09-23 13:36:57 +0900

    feat : add test for module01-ex00

--- 01/ex00/main.cpp
+++ 01/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 13:17:32 by dogwak            #+#    #+#             */
-/*   Updated: 2024/09/23 13:17:48 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/09/23 13:36:42 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 int main()
 {
-	
+	Zombie z("dogwak, stack allocated");
+	z.announce();
+
+	Zombie *zPtr = newZombie("dogwak, heap allocated");
+	zPtr->announce();
+
+	// random chump created, announce, deleted
+	randomChump("dogwak, random chump");
+
+	// heap zombie deleted
+	delete zPtr;
+	zPtr = NULL;
+
+	// stack zombie deleted, compiler will insert the destructor
 }
