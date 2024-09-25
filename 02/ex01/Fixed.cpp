0db1868d66e5b959b36477ae5b8a411f3adf24ba
iphuck22 <eldeshue@naver.com>
2024-09-25 20:56:15 +0900

    feat : implement cpp02ex01.

---
+++ 02/ex01/Fixed.cpp
@@ -0,0 +1,76 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 18:08:46 by dogwak            #+#    #+#             */
+/*   Updated: 2024/09/25 19:56:17 by dogwak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.h"
+
+const int Fixed::binaryPoint = 8;
+
+Fixed::Fixed() : bitData(0)
+{
+	std::cout << "Default constructor called.\n";
+}
+
+Fixed::Fixed(const int i) : bitData(i << binaryPoint)
+{
+	std::cout << "Int constructor called.\n";
+}
+
+Fixed::Fixed(const float f) : bitData(f * static_cast<float>(1 << binaryPoint))
+{
+	std::cout << "Float constructor called.\n";
+}
+
+Fixed::Fixed(const Fixed &f)
+{
+	std::cout << "Copy constructor called.\n";
+	*this = f;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called.\n";
+}
+
+Fixed &Fixed::operator=(const Fixed &rhs)
+{
+	std::cout << "Copy assignment operator called.\n";
+	if (this != &rhs)
+		this->bitData = rhs.getRawBits();
+	return *this;
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called.\n";
+	return bitData;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called.\n";
+	bitData = raw;
+}
+
+float Fixed::toFloat() const
+{
+	return static_cast<float>(bitData) / static_cast<float>(1 << binaryPoint);
+}
+
+float Fixed::toInt() const
+{
+	return (bitData >> binaryPoint);
+}
+
+std::ostream &operator<<(std::ostream &os, const Fixed &f)
+{
+	return os << f.toFloat();
+}
---
+++ 02/ex01/Fixed.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/25 13:58:09 by dogwak            #+#    #+#             */
+/*   Updated: 2024/09/25 18:36:50 by dogwak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_H
+#define FIXED_H
+
+#include <iostream>
+
+class Fixed
+{
+private:
+	static const int binaryPoint;
+	int bitData;
+
+public:
+	Fixed();
+	Fixed(const int i);
+	Fixed(const float f);
+	Fixed(const Fixed &f);
+	~Fixed();
+
+	// operator overloading
+	Fixed &operator=(const Fixed &rhs);
+
+	// get access
+	int getRawBits() const;
+	void setRawBits(int const raw);
+
+	// explicit converter
+	float toFloat() const;
+	float toInt() const;
+};
+
+// extraction operator function overloading, not a member function.
+std::ostream &operator<<(std::ostream &os, const Fixed &f);
+
+#endif
