f8828d523aef63a1f427c12abd5c8d808af4b3f2
iphuck22 <eldeshue@naver.com>
2024-09-28 15:42:19 +0900

    modify : change header guard.

--- 03/ex02/FragTrap.h
+++ 03/ex02/FragTrap.h
@@ -6,12 +6,12 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 15:26:52 by dogwak            #+#    #+#             */
-/*   Updated: 2024/09/28 15:28:53 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/09/28 15:41:12 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef SCAV_TRAP_H
-#define SCAV_TRAP_H
+#ifndef FRAG_TRAP_H
+#define FRAG_TRAP_H
 
 #include "ClapTrap.h"
 
