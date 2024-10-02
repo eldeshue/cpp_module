41668dd950e1a2a1cf0019ad2ae1d6fc6530048c
iphuck22 <eldeshue@naver.com>
2024-10-02 20:49:16 +0900

    feat : add test for ex03

--- 03/ex03/main.cpp
+++ 03/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/28 13:23:06 by dogwak            #+#    #+#             */
-/*   Updated: 2024/10/02 20:20:26 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/10/02 20:46:52 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,12 +16,44 @@
 int main()
 {
 	DiamondTrap test("test");
-
-	test.attack("target");
+	static_cast<ClapTrap &>(test).takeDamage(0); // show member variables.
 	test.whoAmI();
+	test.attack("target");
 
 	DiamondTrap test2(test);
-
-	test2.attack("target2");
+	static_cast<ClapTrap &>(test).takeDamage(0); // show member variables.
 	test2.whoAmI();
+	test2.attack("target2");
+
+	test.attack("target");
+	test.attack("target");
+
+	// poly morphism
+	test = DiamondTrap("test3");
+	ClapTrap &testClap = test;
+	ScavTrap &testScav = test;
+	FragTrap &testFrag = test;
+
+	testClap.attack("target3");
+	testScav.attack("target3");
+	testFrag.attack("target3");
+
+	testClap.takeDamage(10);
+	testScav.takeDamage(10);
+	testFrag.takeDamage(10);
+
+	testClap.beRepaired(10);
+	testScav.beRepaired(10);
+	testFrag.beRepaired(10);
+
+	testClap.takeDamage(20);
+	testScav.takeDamage(20);
+	testFrag.takeDamage(20);
+
+	testScav.attack("target4");
+	test.attack("target4");
+
+	testScav.guardGate();
+	testFrag.highFivesGuys();
+	test.whoAmI();
 }
