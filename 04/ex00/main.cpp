82044df44a04c4361f69dbd4998dcc8c006b2bb1
iphuck22 <eldeshue@naver.com>
2024-10-07 20:44:48 +0900

    feat : add comment to the tests.

--- 04/ex00/main.cpp
+++ 04/ex00/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:32:41 by dogwak            #+#    #+#             */
-/*   Updated: 2024/10/07 20:15:28 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/10/07 20:44:32 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,28 +21,38 @@
 
 int main()
 {
-	// basic polymorphism
+	// basic polymorphism, pointer to base can call derived's member function. because of virtual
 	const Animal *meta = new Animal();
 	const Animal *j = new Dog();
 	const Animal *i = new Cat();
-	std::cout << j->getType() << " " << std::endl;
-	std::cout << i->getType() << " " << std::endl;
-	i->makeSound();
-	j->makeSound();
-	meta->makeSound();
+	std::cout << j->getType() << " " << std::endl; // cat
+	std::cout << i->getType() << " " << std::endl; // dog
+	i->makeSound();								   // meow expected
+	j->makeSound();								   // bark expected
+	meta->makeSound();							   // default animal sound expected
 	delete meta;
 	delete i;
 	delete j;
 
-	// wrong cat
+	// wrong cat, without virtual, cannot call derived's member founction
 	const WrongAnimal *wMeta = new WrongAnimal();
 	const WrongAnimal *k = new WrongCat();
 	const WrongCat &l = *(static_cast<const WrongCat *>(k));
 	std::cout << k->getType() << " " << std::endl;
 	std::cout << l.getType() << " " << std::endl;
-	k->makeSound();
-	l.makeSound();
+	k->makeSound(); // default wrong animal sound expected
+	l.makeSound();	// wrong meow expected
 
 	delete wMeta;
 	delete k;
+
+	// reference
+	const Dog d;
+	const Animal &refAnimal = d;
+
+	std::cout << d.getType() << std::endl; // Dog expected
+	d.makeSound();						   // Bark expected
+
+	std::cout << refAnimal.getType() << std::endl; // Dog expected
+	refAnimal.makeSound();						   // Bark expected
 }
