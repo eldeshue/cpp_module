a5bac440933a276566cb75fbd27a8b77c5954fb1
iphuck22 <eldeshue@naver.com>
2024-10-09 16:45:08 +0900

    feat : implement test main.

--- 04/ex01/main.cpp
+++ 04/ex01/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/07 17:32:41 by dogwak            #+#    #+#             */
-/*   Updated: 2024/10/07 20:44:32 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/10/09 16:43:50 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,38 +21,15 @@
 
 int main()
 {
-	// basic polymorphism, pointer to base can call derived's member function. because of virtual
-	const Animal *meta = new Animal();
-	const Animal *j = new Dog();
-	const Animal *i = new Cat();
-	std::cout << j->getType() << " " << std::endl; // cat
-	std::cout << i->getType() << " " << std::endl; // dog
-	i->makeSound();								   // meow expected
-	j->makeSound();								   // bark expected
-	meta->makeSound();							   // default animal sound expected
-	delete meta;
-	delete i;
-	delete j;
-
-	// wrong cat, without virtual, cannot call derived's member founction
-	const WrongAnimal *wMeta = new WrongAnimal();
-	const WrongAnimal *k = new WrongCat();
-	const WrongCat &l = *(static_cast<const WrongCat *>(k));
-	std::cout << k->getType() << " " << std::endl;
-	std::cout << l.getType() << " " << std::endl;
-	k->makeSound(); // default wrong animal sound expected
-	l.makeSound();	// wrong meow expected
-
-	delete wMeta;
-	delete k;
-
-	// reference
-	const Dog d;
-	const Animal &refAnimal = d;
-
-	std::cout << d.getType() << std::endl; // Dog expected
-	d.makeSound();						   // Bark expected
-
-	std::cout << refAnimal.getType() << std::endl; // Dog expected
-	refAnimal.makeSound();						   // Bark expected
+	// brain test.
+	Brain b;
+	b[98] = "test";
+	try
+	{
+		b[101] = "error";
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << e.what() << '\n';
+	}
 }
