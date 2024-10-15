a521bb8fe07a56976442c4ec85f1d453fd3d8c64
iphuck22 <eldeshue@naver.com>
2024-10-15 17:17:26 +0900

    fix : change main.

--- 01/ex02/main.cpp
+++ 01/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/23 16:20:40 by dogwak            #+#    #+#             */
-/*   Updated: 2024/09/23 16:34:05 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/10/14 20:51:24 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,15 @@ int main()
 	std::string *stringPTR = &stringVAR;
 	std::string &stringREF = stringVAR;
 
-	std::cout << "print memory addresses.\naddress of string variable : " << &stringVAR
-			  << "\naddress of string pointer : "
-			  << &stringPTR
-			  << "\naddress of string reference : "
+	std::cout << "print memory addresses.\naddress of stringVAR : " << &stringVAR
+			  << "\naddress held by stringPTR : "
+			  << stringPTR
+			  << "\naddress that stringREF reference to : "
 			  << &stringREF
 			  << "\nprint values.\nvalue of string variable : "
 			  << stringVAR
-			  << "\nvalue of string pointer : "
-			  << stringPTR
+			  << "\nvalue of string pointed by stringPTR : "
+			  << *stringPTR
 			  << "\nvalue of string reference : "
 			  << stringREF
 			  << '\n';
