b5280a2302b7cb4ba7c29ea1952f814cb86b680b
iphuck22 <eldeshue@naver.com>
2024-10-15 21:13:34 +0900

    feat : implement main and Make for ex04

---
+++ 04/ex03/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/15 21:10:28 by dogwak            #+#    #+#             */
+/*   Updated: 2024/10/15 21:11:16 by dogwak           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Character.h"
+#include "MateriaSource.h"
+
+#include "Ice.h"
+#include "Cure.h"
+
+int main()
+{
+	IMateriaSource *src = new MateriaSource();
+	src->learnMateria(new Ice());
+	src->learnMateria(new Cure());
+	ICharacter *me = new Character("me");
+	AMateria *tmp;
+	tmp = src->createMateria("ice");
+	me->equip(tmp);
+	tmp = src->createMateria("cure");
+	me->equip(tmp);
+	ICharacter *bob = new Character("bob");
+	me->use(0, *bob);
+	me->use(1, *bob);
+	delete bob;
+	delete me;
+	delete src;
+}
