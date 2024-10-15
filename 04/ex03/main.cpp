b22d9a312151df0998d00f46cf35d17db4ab0900
iphuck22 <eldeshue@naver.com>
2024-10-15 21:42:26 +0900

    minor fix

--- 04/ex03/main.cpp
+++ 04/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: dogwak <dogwak@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/15 21:10:28 by dogwak            #+#    #+#             */
-/*   Updated: 2024/10/15 21:11:16 by dogwak           ###   ########.fr       */
+/*   Updated: 2024/10/15 21:33:56 by dogwak           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,15 +21,20 @@ int main()
 	IMateriaSource *src = new MateriaSource();
 	src->learnMateria(new Ice());
 	src->learnMateria(new Cure());
+
 	ICharacter *me = new Character("me");
+
 	AMateria *tmp;
 	tmp = src->createMateria("ice");
 	me->equip(tmp);
 	tmp = src->createMateria("cure");
 	me->equip(tmp);
+
 	ICharacter *bob = new Character("bob");
+
 	me->use(0, *bob);
 	me->use(1, *bob);
+
 	delete bob;
 	delete me;
 	delete src;
