afda8523b37e985accd1d7350577a7beaa8922b7
iphuck22 <eldeshue@naver.com>
2024-11-11 16:24:39 +0900

    feat : implement PresidentialPardonForm.

---
+++ 05/ex02/PresidentialPardonForm.cpp
@@ -0,0 +1,43 @@
+
+#include "PresidentialPardonForm.h"
+
+PresidentialPardonForm::PresidentialPardonForm(const std::string &target)
+try : AForm(PPF_TYPE_NAME, PPF_SIGN_GRADE, PPF_EXEC_GRADE), target(target)
+{
+}
+catch (const std::exception &e)
+{
+	throw;
+}
+
+PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &other)
+try : AForm(other), target(other.target)
+{
+}
+catch (const std::exception &e)
+{
+	throw;
+}
+
+PresidentialPardonForm::~PresidentialPardonForm()
+{
+}
+
+PresidentialPardonForm &PresidentialPardonForm::operator=(const PresidentialPardonForm &rhs)
+{
+	this->target = rhs.target;
+}
+
+void PresidentialPardonForm::execute(Bureaucrat const &executor) const
+{
+	try
+	{
+		checkExecutable(executor);
+
+		std::cout << target << " has been pardoned by Zaphod Beeblebrox.\n";
+	}
+	catch (const std::exception &e)
+	{
+		throw;
+	}
+}
---
+++ 05/ex02/PresidentialPardonForm.h
@@ -0,0 +1,26 @@
+
+#ifndef PRESIDENTIAL_PARDON_FORM
+#define PRESIDENTIAL_PARDON_FORM
+
+#include "AForm.h"
+
+#define PPF_TYPE_NAME "SCHRUBBERY CREATION FORM"
+#define PPF_SIGN_GRADE 25
+#define PPF_EXEC_GRADE 5
+
+class PresidentialPardonForm : public AForm
+{
+private:
+	std::string target;
+
+public:
+	PresidentialPardonForm(const std::string &target);
+	PresidentialPardonForm(const PresidentialPardonForm &other);
+	~PresidentialPardonForm();
+
+	PresidentialPardonForm &operator=(const PresidentialPardonForm &rhs);
+
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
