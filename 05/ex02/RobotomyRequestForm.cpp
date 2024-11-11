eedfe582c14c02afc20700added61ec94ab330dd
iphuck22 <eldeshue@naver.com>
2024-11-11 16:14:23 +0900

    feat : RobotomyRequestForm implemented.

---
+++ 05/ex02/RobotomyRequestForm.cpp
@@ -0,0 +1,56 @@
+
+#include "RobotomyRequestForm.h"
+#include <fstream>
+#include <ctime>
+#include <cstdlib>
+
+RobotomyRequestForm::RobotomyRequestForm(const std::string &target)
+try : AForm(RRF_TYPE_NAME, RRF_SIGN_GRADE, RRF_EXEC_GRADE), target(target)
+{
+}
+catch (const std::exception &e)
+{
+	throw;
+}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &other)
+try : AForm(other), target(other.target)
+{
+}
+catch (const std::exception &e)
+{
+	throw;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm()
+{
+}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &rhs)
+{
+	this->target = rhs.target;
+}
+
+void RobotomyRequestForm::execute(Bureaucrat const &executor) const
+{
+	try
+	{
+		checkExecutable(executor);
+
+		// do something
+		std::cout << "Makes som drilling noises...\n";
+		srand(time(NULL));
+		if (rand() & 1)
+		{
+			std::cout << target << " has been robotomized successfully.\n";
+		}
+		else
+		{
+			std::cout << "Robotomizing " << target << " failed.\n";
+		}
+	}
+	catch (const std::exception &e)
+	{
+		throw;
+	}
+}
---
+++ 05/ex02/RobotomyRequestForm.h
@@ -0,0 +1,26 @@
+
+#ifndef ROBOTOMY_REQUEST_FORM
+#define ROBOTOMY_REQUEST_FORM
+
+#include "AForm.h"
+
+#define RRF_TYPE_NAME "ROBOTOMY REQUEST FORM"
+#define RRF_SIGN_GRADE 72
+#define RRF_EXEC_GRADE 45
+
+class RobotomyRequestForm : public AForm
+{
+private:
+	std::string target;
+
+public:
+	RobotomyRequestForm(const std::string &target);
+	RobotomyRequestForm(const RobotomyRequestForm &other);
+	~RobotomyRequestForm();
+
+	RobotomyRequestForm &operator=(const RobotomyRequestForm &rhs);
+
+	virtual void execute(Bureaucrat const &executor) const;
+};
+
+#endif
