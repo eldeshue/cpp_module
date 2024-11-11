9670db752ef718f4c02ef917e606de500b2567c8
iphuck22 <eldeshue@naver.com>
2024-11-11 21:22:21 +0900

    feat : add test main for cpp05ex03.

--- 05/ex03/main.cpp
+++ 05/ex03/main.cpp
@@ -7,6 +7,30 @@
 #include "RobotomyRequestForm.h"
 #include "PresidentialPardonForm.h"
 
+#include "Intern.h"
+
 int main()
 {
+	Intern i;
+	Bureaucrat master(1, "Master_Beureaucrat");
+	AForm *forms[4];
+
+	forms[0] = i.makeForm("SHRUBBERY_CREATION_FORM", "target1");
+	forms[1] = i.makeForm("ROBOTOMY_REQUEST_FORM", "target2");
+	forms[2] = i.makeForm("PRESIDENTIAL_PARDON_FORM", "target3");
+	forms[3] = i.makeForm("UNKNOWN_FORM", "target4");
+
+	for (int i = 0; i < 4; ++i)
+	{
+		if (forms[i] != NULL)
+		{
+			master.signForm(*forms[i]);
+			master.executeForm(*forms[i]);
+		}
+	}
+
+	for (int i = 0; i < 4; ++i)
+	{
+		delete forms[i];
+	}
 }
