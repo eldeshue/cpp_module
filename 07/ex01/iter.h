cee49dc8cd0c28dbb9e9e1e95e2f76cf31a50e17
iphuck22 <eldeshue@naver.com>
2025-02-03 10:19:43 +0900

    feat : implement cpp07ex01

---
+++ 07/ex01/iter.h
@@ -0,0 +1,14 @@
+
+#ifndef ITER_H
+#define ITER_H
+
+#include <cstddef>
+
+template<typename T, typename UnaryOps>
+void iter(T* arr, size_t len, UnaryOps f)
+{
+	for (int i = 0; i < len; ++i)
+		f(arr[i]);
+}
+
+#endif
