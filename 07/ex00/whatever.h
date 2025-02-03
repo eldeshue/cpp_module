b7a5cc8e2831ab4ffa52cbf85b56c5b892a01cab
iphuck22 <eldeshue@naver.com>
2025-02-03 09:57:30 +0900

    feat : cpp07ex00

---
+++ 07/ex00/whatever.h
@@ -0,0 +1,34 @@
+
+#ifndef WHATEVER_H
+#define WHATEVER_H
+
+// swap, copy assign operator needed
+template<typename T>
+void swap(T val1, T val2)
+{
+	T tmp = val1;
+	val1 = val2;
+	val2 = tmp;
+}
+
+// min, copy-assing and compare operator needed
+template<typename T>
+T min(T val1, T val2)
+{
+	if (val1 < val2)
+		return val1;
+	else // val1 >= val2
+		return val2;
+}
+
+// max, copy-assing and compare operator needed
+template<typename T>
+T max(T val1, T val2)
+{
+	if (val1 > val2)
+		return val1;
+	else // val1 <= val2
+		return val2;
+}
+
+#endif
